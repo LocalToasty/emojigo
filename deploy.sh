36ead4b93e662b5113e2b682a1120c50b825ac6a
Marko van Treeck <markovantreeck@gmail.com>
2017-11-28 17:36:37 +0100

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git checkout gh-pages &&
     git merge --no-edit master &&
     make index.md &&
     git add *.md &&
-    git commit -m "Deploy $(LC_TIME=en_US.UTF8 date)" &&
-    git push
+    git commit -m "Deploy $(LC_TIME=en_US.UTF8 date)"
+git push
 git co master
